7103d0cff69ef18d901ffabbe22dd0d0ea57b0b4
engelmeierdaniel <daniel_engelmeier@hotmail.com>
2025-03-13 20:29:07 -0300

    Desafio Curso Alura Java listas e coleções de dados

--- src/br/com/danielcompany/desafiocursolistascolecoesdados/modelos/Titulo.java
+++ src/br/com/danielcompany/desafiocursolistascolecoesdados/modelos/Titulo.java
@@ -1,4 +1,22 @@
 package br.com.danielcompany.desafiocursolistascolecoesdados.modelos;
 
-public class Titulo {
+public class Titulo implements Comparable<Titulo> {
+    private String Titulo;
+
+    public Titulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    @Override
+    public int compareTo(Titulo outro) {
+        return this.getTitulo().compareTo(outro.getTitulo());
+    }
 }
--- src/br/com/danielcompany/desafiocursolistascolecoesdados/principal/Main.java
+++ src/br/com/danielcompany/desafiocursolistascolecoesdados/principal/Main.java
@@ -1,5 +1,10 @@
+package br.com.danielcompany.desafiocursolistascolecoesdados.principal;
+
+import br.com.danielcompany.desafiocursolistascolecoesdados.modelos.Titulo;
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 //1.Crie uma lista de números inteiros e utilize o
@@ -10,6 +15,16 @@ import java.util.List;
 //  Implemente a interface Comparable na classe para que seja possível
 //  ordenar uma lista de objetos Titulo.
 
+//3.No Exercício 2, crie alguns objetos da classe Titulo e adicione-os a
+//  uma lista. Utilize o método Collections.sort para ordenar a lista e,
+//  em seguida, imprima os títulos ordenados.
+
+//4.Crie uma lista utilizando a interface List e instancie-a tanto como
+//  ArrayList quanto como LinkedList. Adicione elementos e imprima a lista,
+//  mostrando que é possível trocar facilmente a implementação.
+
+//5.Modifique o Exercício 4 para declarar a variável de lista como a
+//  interface List, demonstrando o uso de polimorfismo.
 
 public class Main {
     public static void main(String[] args) {
@@ -23,5 +38,28 @@ public class Main {
 
         Collections.sort(numeros);
         System.out.println(numeros);
+
+        Titulo livro1 = new Titulo("Crônicas de Artur");
+        Titulo livro2 = new Titulo("Fundação");
+        Titulo livro3 = new Titulo("O mundo perdido");
+        Titulo livro4 = new Titulo("Duna");
+
+        List<Titulo> livros = new ArrayList<>();
+        livros.add(livro1);
+        livros.add(livro2);
+        livros.add(livro3);
+        livros.add(livro4);
+
+        Collections.sort(livros);
+
+        //List<String> nomes = new ArrayList<>(); Ex 4 e 5 trocando...
+        List<String> nomes = new LinkedList<>();
+        nomes.add("Roger");
+        nomes.add("Rodolfo");
+        nomes.add("Rodrigo");
+        nomes.add("Renato");
+        nomes.add("Reginaldo");
+
+        System.out.println(nomes);
     }
 }
